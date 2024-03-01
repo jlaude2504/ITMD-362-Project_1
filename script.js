652e06a185ad11751f274e41f8e3304819677402
jlaude2504 <jlaude@hawk.iit.edu>
2024-02-29 20:17:56 -0600

    Setup Event

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('signup-form');
+  
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      
+     
+    });
+  });
